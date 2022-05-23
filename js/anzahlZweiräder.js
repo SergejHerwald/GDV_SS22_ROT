26b846a59b02f274cb7e4175b22b554e1d55fe9d
1827278 <44192275+1827278@users.noreply.github.com>
2022-05-23 18:16:39 +0200

    Ganzes Repo

--- js/anzahlZweiräder.js
+++ js/anzahlZweiräder.js
@@ -1,44 +1,48 @@
-var summeZweirad = echarts.init(document.getElementById('summeZweirad'));
+var summeZweirad = echarts.init(document.getElementById("summeZweirad"));
 
 let sum = [];
-function getSumZweirad2(tsID, startDate, endDate){
-  $.getJSON(`https://api.mvvsmartcities.com/v3/timeseries?Ocp-Apim-Subscription-Key=8e3b5fe2c8644919ae63394238b89644&timeSeriesId=${tsID}&from=${startDate}&to=${endDate}&func=sum&interval=d&timezone=Europe%2FBerlin&sort=asc&output=records&metadata=false`, {}, function (res) {
-    //for (let i of res[0].records) {
+function getSumZweirad2(tsID, startDate, endDate) {
+  $.getJSON(
+    `https://api.mvvsmartcities.com/v3/timeseries?Ocp-Apim-Subscription-Key=8e3b5fe2c8644919ae63394238b89644&timeSeriesId=${tsID}&from=${startDate}&to=${endDate}&func=sum&interval=d&timezone=Europe%2FBerlin&sort=asc&output=records&metadata=false`,
+    {},
+    function (res) {
       sum.push(res[0]);
-    //}
-  });
+    }
+  );
   return sum;
 }
 
-
 let sumFahrr = [];
-let startDate= '2022-04-01T00%3A00%3A00.000Z';
-let endDate = '2022-05-01T00%3A00%3A00.000Z';
+let startDate = "2022-04-01T00%3A00%3A00.000Z";
+let endDate = "2022-05-01T00%3A00%3A00.000Z";
 let temp1 = [];
-function getData(fahrr,motor){
-    sumFahrr = getSumZweirad2(fahrr[0].timeSeriesId, startDate, endDate);
-    console.log(sumFahrr);
+function getData(fahrr, motor) {
+  sumFahrr = getSumZweirad2(fahrr[0].timeSeriesId, startDate, endDate);
 }
 
-  summeZweirad.setOption(option={
-  
+summeZweirad.setOption(
+  (option = {
     title: {
-      text: 'Mannheim',
-      left: 'center',
+      text: "Mannheim",
+      left: "center",
       textStyle: {
-        color: 'black'
-      }
+        color: "black",
+      },
     },
     xAxis: {
-      type: 'category',
-      data: ["hallo", "Test"]
+      type: "category",
     },
     yAxis: {
-      type: 'value',
+      type: "value",
     },
-    series: [{
-        type: 'line',
-        data: [10, 20]
+    series: [
+      {
+        type: "line",
+        data: [
+          ["monatag", 10],
+          ["dienstag", 20],
+        ],
       },
-    ]
-  });
+    ],
+  })
+);
