6a9a65fc54e7ff968b29018e3dd842934312881c
1827278 <44192275+1827278@users.noreply.github.com>
2022-06-22 21:13:54 +0200

    Merge

--- js/script.js
+++ js/script.js
@@ -94,27 +94,27 @@ function buildMap(geoCoordMap,data){
     ],
   });
   myChart.on('click',(params)=>{
-    console.log(params)
+    doWork(params.data.name.toLowerCase().replace(" ",""))
   })
 
-  (function () {
-    var throttle = function (type, name, obj) {
-      obj = obj || window;
-      var running = false;
-      var func = function () {
-        if (running) {
-          return;
-        }
-        running = true;
-        requestAnimationFrame(function () {
-          obj.dispatchEvent(new CustomEvent(name));
-          running = false;
-        });
-      };
-      obj.addEventListener(type, func);
-    };
-    throttle("resize", "optimizedResize");
-  })();
+  // (function () {
+  //   var throttle = function (type, name, obj) {
+  //     obj = obj || window;
+  //     var running = false;
+  //     var func = function () {
+  //       if (running) {
+  //         return;
+  //       }
+  //       running = true;
+  //       requestAnimationFrame(function () {
+  //         obj.dispatchEvent(new CustomEvent(name));
+  //         running = false;
+  //       });
+  //     };
+  //     obj.addEventListener(type, func);
+  //   };
+  //   throttle("resize", "optimizedResize");
+  // })();
 
   window.addEventListener("optimizedResize", function () {
     myChart.resize({
